88d2b81d62e17fa322b4f3bf7e3544b881bd4a4b
Elliot <me@robiot.dev>
2024-03-16 23:31:34 +0000

    appid

--- src/lib/enviroment.ts
+++ src/lib/enviroment.ts
@@ -1,3 +1,3 @@
 export const enviroment = {
-  WI_APP_ID: "app_staging_c1b712d4cbcf6127a1750e851d9d4540" as `app_${string}`,
+  WI_APP_ID: process.env.NEXT_PUBLIC_WI_APP_ID ?? "app_staging_c1b712d4cbcf6127a1750e851d9d4540" as `app_${string}`,
 };
